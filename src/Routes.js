02f3ab8eb19c6200fc3a24af0c49e4ed2e6fd502
Guss-droid <gustavo.re@inforoute.com.br>
2021-02-26 11:32:34 -0300

    publicando outra pagina

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import Paulista from './pages/Paulista'
+import Paulista from './pages/Paulista';
+import Mineiro from './pages/Mineiro';
 
 export default function Routes(){
   return(
       <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route path="/Truco-Paulista" component={Paulista} />
+        <Route path="/Paulista" component={Paulista} />
+        <Route path="/Mineiro" component={Mineiro} />
       </Switch>
       </BrowserRouter>
   )
