bbb94a07fb6c43b2cc33597c56bf18950581d3c9
Guss-droid <gustavo.re@inforoute.com.br>
2021-02-26 11:30:03 -0300

    Algumas alteraçoes

--- src/pages/Mineiro/index.js
+++ src/pages/Mineiro/index.js
@@ -4,7 +4,7 @@ import { darkTheme, lightTheme } from '../../Temas/theme';
 import GlobalTheme from '../../Temas/globals';
 // import '../layouts/Paulista.css';
 
-export default function Paulista () {
+export default function Mineiro() {
 
     const [theme, setTheme] = useState('light')
 
@@ -54,11 +54,11 @@ export default function Paulista () {
                     <Title>
                         <div>
                             <ButtonChange className="btn-mudou" onClick={trocaTema}>Mudar tema </ButtonChange>
-                            <h2>Regras do Truco Paulista</h2>
+                            <h2>Regras do Truco Mineiro</h2>
                             <hr />
                             <p>
-                                Cada jogador recebe três cartas que são de um subconjunto do baralho constituído pelos números 1 a 7, a dama,
-                                o valete, e o reis.
+                       Cada jogador recebe três cartas que são de um subconjunto do baralho constituído pelos números 1 a 7, a dama,
+                      o valete, e o reis.
             <br />
                                 <br />
             A forma mais comum do jogo é a versão com quatro jogadores,
@@ -73,50 +73,50 @@ export default function Paulista () {
                                 <br />
                                 <hr />
                                 <h3>Manilhas</h3>
-                As manilhas não são fixas sempre irá depender do vira
-                <br /> Ex: se o vira for o 4 a manilha é o 5 e assim por diante
+                As manilhas são fixas sendo elas o 4 de Paus, o 7 de Copas,
+                o Az de Espada e o 7 de Ouros
                 <hr />
                                 <h3>Pontos</h3>
                                 <hr />
-                O jogo é disputado em mãos. Cada mão vale inicialmente 1 ponto, e ganha o jogo quem fizer 12 pontos primeiro.
+                O jogo é disputado em mãos. Cada mão vale inicialmente 2 ponto, e ganha o jogo quem fizer 12 pontos primeiro.
                 A mão é dividida em três rodadas . Em cada rodada cada jogador coloca uma de suas cartas na mesa,
-                e o jogador com a carta mais forte vence a rodada. Quem ganhar duas dessas rodadas ganha a mão e marca 1 ponto,
+                e o jogador com a carta mais forte vence a rodada. Quem ganhar duas dessas rodadas ganha a mão e marca 2 ponto,
                 e uma nova mão se inicia. Em caso de empate na primeira rodada, o vencedor da segunda é o vencedor, se empatar
                 na segunda o vencedor da terceira é o vencedor . Em caso de empate na segunda ou terceira rodada,
                 o vencedor da primeira é o vencedor. A qualquer hora o jogador pode pedir Truco. É o grande momento do jogo.
-                O Truco é pedido para elevar a aposta a três tentos .
+                O Truco é pedido para elevar a aposta a quatro tentos .
                 <br />
                                 <h4>Ao ser trucada, a dupla adversária tem direito a três ações: </h4>
                                 <hr />
-                Mandar cair: a mão passa a valer 3 tentos.
+                Mandar cair: a mão passa a valer 4 tentos.
                 <br />
-                Pedir 6: a mão passa a valer 6 tentos.
+                Pedir 6: a mão passa a valer 8 tentos.
                 <br />
-                Fugir: a dupla que trucou leva 1 tento.
+                Fugir: a dupla que trucou leva 2 tento.
                 <br />
                                 <hr />
                                 <h4 > Caso a dupla 'B' pedir 6 a dupla 'A' pode: </h4>
                                 <hr />
-                Mandar cair: a mão passa a valer 6 tentos.
+                Mandar cair: a mão passa a valer 8 tentos.
                 <br />
-                Pedir 9: a mão passa a valer 9 tentos.
+                Pedir 9: a mão passa a valer 10 tentos.
                 <br />
-                Fugir: a dupla que chamou 6 leva 3 tentos.
+                Fugir: a dupla que chamou 6 leva 4 tentos.
                 <hr />
                                 <h4>Caso a dupla 'A' peça nove : </h4>
                                 <hr />
-                Mandar cair: a mão passa a valer 9 tentos.
+                Mandar cair: a mão passa a valer 10 tentos.
                 <br />
                 Pedir 12: a mão passa a valer o jogo quem ganhar ganha tudo.
                 <br />
-                Fugir: a dupla que chamou nove leva 6 tentos.
+                Fugir: a dupla que chamou nove leva 8 tentos.
                 <br />
                                 <hr />
                                 <h4>Se a dupla 'B' chamar 12 a dupla 'A' pode: </h4>
                                 <hr />
                 Manda cair: a mão passa a valer o jogo quem ganhar ganha tudo
                 <br />
-                Fugir: a dupla que chamou 12 leva 9 tentos
+                Fugir: a dupla que chamou 12 leva 10 tentos
                 </p>
                             <hr />
                             <section className="Add-body">
