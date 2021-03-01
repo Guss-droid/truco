6efeb94f3e19b015883e7010aae96c27afbd12bb
Guss-droid <gustavo.re@inforoute.com.br>
2021-03-01 14:08:23 -0300

    Mudando fonte

--- src/pages/MarcadorP/index.js
+++ src/pages/MarcadorP/index.js
@@ -37,6 +37,61 @@ export default function MarcadorP() {
 
     const [partidasTime2, setPartidasTime2] = useState(0)
 
+    const sinais = [
+        {
+            Zap: 'Pisque o olho',
+            id: 1
+        },
+        {
+            Copas: 'Levante a sobrancelha',
+            id: 2
+        },
+        {
+            Espada: 'Movimente a bochecha',
+            id: 3
+        },
+        {
+            Ouros: 'Mostre a lingua',
+            id: 4
+        },
+        {
+            Tres: 'Movimente o ombro',
+            id: 5
+        },
+        {
+            Dois: 'Vire a cabeça',
+            id: 6
+        },
+        {
+            Áz: 'Levante o mindinho',
+            id: 7
+        },
+        {
+            Reis: 'Passe a mão no queixo',
+            id: 8
+        },
+        {
+            Valete: 'Um jota com a mão',
+            id: 9
+        },
+        {
+            Dama: 'Mão no peito',
+            id: 10
+        },
+        {
+            Sete: 'Cara feia',
+            id: 11
+        }
+    ]
+
+    const [message, setMessage] = useState()
+
+    function onClick(e) {
+        setMessage(
+           e.target.value
+        )
+    }
+
     function onTruco() {
         localStorage.setItem("truco", truco)
         localStorage.removeItem("seis")
@@ -108,10 +163,6 @@ export default function MarcadorP() {
     }
 
     function juntaTentos() {
-        localStorage.removeItem("truco")
-        localStorage.removeItem("seis")
-        localStorage.removeItem("nove")
-        localStorage.removeItem("doze")
         setContador(
             contador + 1
         )
@@ -256,11 +307,13 @@ export default function MarcadorP() {
                         <div className="App">
                             <h2>
                                 Time 1
-                                <br />
+                                </h2>
+                            <hr />
+                            <h1>
                                 Nós: {contador}
                                 <br />
                                 Partidas ganhas: {partidas}
-                            </h2>
+                            </h1>
                             <button className="btn-Add-Pts" onClick={incrementaPontos}>Adicionar Pontos</button>
                             <hr />
                             <br />
@@ -277,12 +330,37 @@ export default function MarcadorP() {
                             <hr />
                             <h2>
                                 Time 2
-                                <br />
+                                </h2>
+                            <hr />
+                            <h1>
                                 Eles: {contadorTime2}
                                 <br />
                                 Partidas ganhas: {partidasTime2}
-                            </h2>
+                            </h1>
                             <button className="btn-Add-Pts" onClick={adicionaPontos}>Adicionar pontos</button>
+                            <hr />
+                            <h2>Sinais</h2>
+                            <select onChange={onClick}>
+                                <option id="0" value="o" >Selecione um sinal</option>
+                                <option id="1" value={sinais[0].Zap}>Zap</option>
+                                <option id="2" value={sinais[1].Copas}>Copas</option>
+                                <option id="3" value={sinais[2].Espada}>Espada</option>
+                                <option id="4" value={sinais[3].Ouros}>Ouros</option>
+                                <option id="5" value={sinais[4].Tres}>Três</option>
+                                <option id="6" value={sinais[5].Dois}>Dois</option>
+                                <option id="7" value={sinais[6].Áz}>Áz</option>
+                                <option id="8" value={sinais[7].Reis}>Reis</option>
+                                <option id="9" value={sinais[8].Valete}>Valete</option>
+                                <option id="10" value={sinais[9].Dama}>Dama</option>
+                                <option id="11" value={sinais[10].Sete}>Do sete para baixo</option>
+                            </select>
+                            <div>
+                                {message && (
+                                    <div>
+                                        <h3>{message}</h3>
+                                    </div>
+                                )}
+                            </div>
                             <hr />
                             <br />
                             <section>
